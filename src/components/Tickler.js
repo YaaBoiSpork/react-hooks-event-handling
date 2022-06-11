93ce568f744cb71c4ecdeb93bea1c3ad9f48761c
Stephen Nerby <stephen.a.nerby@gmail.com>
2022-06-10 20:03:26 -0400

    commit tree e751e8e0ff82bc415cffb7cbf6824c27a0c4ef59

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
-  }
-
-  return <button>Tickle me!</button>;
+  return <button onClick={() => console.log("Teehee!")}>Tickle Me!</button>
 }
+
+//function Tickler() {
+//  function tickle() {
+//    console.log("Teehee!");
+//  }
+//
+//  return <button onClick={tickle}>Tickle me!</button>;
+//}
 
 export default Tickler;
